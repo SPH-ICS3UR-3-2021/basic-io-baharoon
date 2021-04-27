835c4872e4396826ff7a2d579698602ec58cb45e
baharoon <mohammed.ba-haroon@ocsbstudent.ca>
2021-04-27 11:23:07 -0400

    Finished

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,12 +5,36 @@
  */
 package main;
 
+import java.util.Scanner;
+
 /**
  *
  * @author k_zeb
  */
 public class Main {
     public static void main(String[] args){
+        
+        // TODO code application logic here
+             String name="Mohammed";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hi! What's your name?");
+        name=in.nextLine();
+        System.out.println("Hi "+name+"! Nice to meet you!");
+        System.out.println("How old are you?");
+        int fav = in.nextInt();
+        System.out.println("You are "+fav+" years old?");
+        System.out.println("You look much younger!");
+        String colour=in.nextLine();
+System.out.println("Ok, goodbye!");
+         
+        
+        in.close()
+    }
+    
+    }
+    
+        
+        
         
     }
     
